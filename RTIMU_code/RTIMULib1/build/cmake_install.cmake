9d47db6a81f9da13ffb05574c771a09129ee76b6
Ashwin Parambath <lpashwin@gmail.com>
2017-01-20 00:32:05 -0600

    rearranged rtmulib1

---
+++ RTIMU_code/RTIMULib1/build/cmake_install.cmake
@@ -0,0 +1,80 @@
+# Install script for directory: /home/pi/RTIMU_code/RTIMULib/RTIMULib
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  foreach(file
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libRTIMULib.so.7.2.1"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libRTIMULib.so.7"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libRTIMULib.so"
+      )
+    if(EXISTS "${file}" AND
+       NOT IS_SYMLINK "${file}")
+      file(RPATH_CHECK
+           FILE "${file}"
+           RPATH "")
+    endif()
+  endforeach()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE SHARED_LIBRARY FILES
+    "/home/pi/RTIMU_code/RTIMULib/RTIMULib/build/libRTIMULib.so.7.2.1"
+    "/home/pi/RTIMU_code/RTIMULib/RTIMULib/build/libRTIMULib.so.7"
+    "/home/pi/RTIMU_code/RTIMULib/RTIMULib/build/libRTIMULib.so"
+    )
+  foreach(file
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libRTIMULib.so.7.2.1"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libRTIMULib.so.7"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libRTIMULib.so"
+      )
+    if(EXISTS "${file}" AND
+       NOT IS_SYMLINK "${file}")
+      if(CMAKE_INSTALL_DO_STRIP)
+        execute_process(COMMAND "/usr/bin/strip" "${file}")
+      endif()
+    endif()
+  endforeach()
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "/home/pi/RTIMU_code/RTIMULib/RTIMULib/." FILES_MATCHING REGEX "/[^/]*\\.h$")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+file(WRITE "/home/pi/RTIMU_code/RTIMULib/RTIMULib/build/${CMAKE_INSTALL_MANIFEST}" "")
+foreach(file ${CMAKE_INSTALL_MANIFEST_FILES})
+  file(APPEND "/home/pi/RTIMU_code/RTIMULib/RTIMULib/build/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+endforeach()
